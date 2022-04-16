cb000d7bc18fa3705f3e79ca740f4ea596bf960b
mdkawsarali <facebookerjordan@gmail.com>
2022-04-17 02:19:27 +0600

    Not Found Added!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotFoundImg from "../../images/not-found.png";
+
+const NotFound = () => {
+    return (
+        <div className='container text-center p-5'>
+            <img src={NotFoundImg} className="w-75" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
